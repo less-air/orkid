6bc71b1e89e80b41974010d724a793cece7b55c2
less-air <nirojassa@gmail.com>
2025-02-25 16:26:12 +0000

    Update pureplace.js

--- pureplace.js
+++ pureplace.js
@@ -97,7 +97,7 @@ function renderFrame() {
   const mouseY = canvas.mouseY || 0;
 
   // Set the radius of the circle that will control the opacity
-  const radius = 50 * (loudness / 100); // The cursor radius grows based on the loudness
+  const radius = 200 * (loudness / 100); // The cursor radius grows based on the loudness
 
   // Draw organic, scattered blobs based on frequency data
   for (let i = 0; i < bufferLength; i++) {
@@ -117,7 +117,7 @@ function renderFrame() {
     const dist = Math.sqrt(Math.pow(mouseX - xPos, 2) + Math.pow(mouseY - yPos, 2));
 
     // Adjust the opacity of the blob based on the distance from the cursor
-    let blobOpacity = 0.01;
+    let blobOpacity = 0.05;
     if (dist < radius) {
       // If the blob is within the radius, adjust opacity based on proximity
       blobOpacity = opacity * (1 - dist / radius);
